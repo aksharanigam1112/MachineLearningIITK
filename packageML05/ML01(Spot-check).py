68ee33a34fb0febae661b2ddea6227796b28eaac
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2019-07-07 17:15:35 +0530

    Create ML01(Spot-check).py

---
+++ packageML05/ML01(Spot-check).py
@@ -0,0 +1,13 @@
+# Spot check ML Algo
+#
+# 1) Logistic Regression
+# 2) Linear Discriminant Analysis(LDA)
+# 3) k-Nearest Neighbours
+# 4) Naive Bayes
+# 5) Classification & Regression Trees(CART)
+# 6) Support Vector Machine
+# 7) Linear SVC
+# 8) Preceptron
+# 9) Random Forest
+# 10) Stochastic Gradient Descent
+3 11) Gradient Boosting Claasifier
